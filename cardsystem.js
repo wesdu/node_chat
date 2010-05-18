1d3714aac4f89d52e4b60e2cae83d7e88a91cbe0
wesdu <pandorid@gmail.com>
2010-05-19 02:58:44 +0800

    Signed-off-by: wesdu <pandorid@gmail.com>

--- cardsystem.js
+++ cardsystem.js
@@ -2,9 +2,12 @@
 	var _Card,
 		_ControlPad,
 		CONFIG;
+		
 	CONFIG={};
 	CONFIG.maxID= 1;
 	CONFIG.zIndex= 1;
+	CONFIG.cards={};
+	//obj包装成Class
 	var Class= function(obj) {
 		var newClass = function() {
 			this.init.apply(this, arguments);
@@ -16,10 +19,10 @@
 		init:function(host,type){
 			this.type= type;
 			this.host= host;
-			this.buildDom();
-			this.buildEvent();
+			(this["buildDom_"+type])();
+			(this["buildEvent_"+type])();
 		},
-		buildDom:function() {
+		buildDom_text:function() {
 			this.el={};
 			this.el.applyB= $(document.createElement("div")); 
 			this.el.pad= $(document.createElement("div"));
@@ -29,8 +32,23 @@
 			applyB.html("Apply");
 			pad.addClass("control_pad");
 			pad.html('<div class="content">\
-				width:<input type="text" value='+this.host._width+' />\
-				height:<input type="text" value='+this.host._height+' />\
+					<div class="tab">\
+					width:<input type="text" value=' + this.host._width + ' />\
+					height:<input type="text" value=' + this.host._height + ' />\
+					</div>\
+					<div class="tab">\
+					size:<input type="text" value="13" />\
+					</div>\
+					<div class="tab">\
+					color:\
+					<h3 class="button orange" ></h3>\
+					<h3 class="button blue" ></h3>\
+					<h3 class="button green" ></h3>\
+					<h3 class="button black" ></h3>\
+					</div>\
+					<div class="tab">\
+					<textarea class="text_field" ></textarea>\
+					</div>\
 			</div>');
 			this.setSize();
 			this.setZIndex();
@@ -38,13 +56,109 @@
 			pad.append(applyB);
 			$('body').after(pad);
 		},
-		buildEvent:function() {
+		buildEvent_text:function() {
 			var _this= this;
+			var colorButtons= this.el.pad.find("h3");
+			var colors= ["#F7921D","#0096CF","#699E21","#3A3A3A"];
+			var color;
+			$(colorButtons).each(function(i,dom){
+				$(dom).click(function(e){	
+					if(_this.__lastColorButton) {
+						$(_this.__lastColorButton).removeClass("select");
+					}
+					$(dom).addClass("select");
+					color= colors[i];
+					_this.__lastColorButton= dom;
+				});
+			});
 			this.el.applyB.click(function(e){
+				//调整大小
 				_this.host._width= parseInt(_this.el.pad.find("input")[0].value,10);
 				_this.host._height= parseInt(_this.el.pad.find("input")[1].value,10);
 				_this.host.setSize();
+				//获取字体大小
+				var fontSize= parseInt(_this.el.pad.find("input")[2].value,10);
+				_this.host._fontSize= fontSize;
+				_this.host.setFontSize();
+				_this.host._fontColor= color;
+				_this.host.setFontColor();
+				_this.host._value= _this.el.pad.find("textarea")[0].value;
+				_this.host.setValue();
 			});
+		},
+		buildDom_img:function() {
+			this.el={};
+			this.el.applyB= $(document.createElement("div")); 
+			this.el.pad= $(document.createElement("div"));
+			var applyB= this.el.applyB;
+			var pad= this.el.pad;
+			applyB.addClass("apply_button");
+			applyB.html("Apply");
+			pad.addClass("control_pad");
+			pad.html('<div class="content">\
+					<div class="tab">\
+					width:<input type="text" value=' + this.host._width + ' />\
+					height:<input type="text" value=' + this.host._height + ' />\
+					</div>\
+					<div class="tab">\
+					url:<input type="text" value="" style="width:170px" />\
+					</div>\
+			</div>');
+			this.setSize();
+			this.setZIndex();
+			this.setPosition();
+			pad.append(applyB);
+			$('body').after(pad);			
+		},
+		buildEvent_img:function() {
+			var _this= this;
+			this.el.applyB.click(function(e){
+				//调整大小
+				_this.host._width= parseInt(_this.el.pad.find("input")[0].value,10);
+				_this.host._height= parseInt(_this.el.pad.find("input")[1].value,10);
+				_this.host.setSize();
+				_this.host._src= _this.el.pad.find("input")[2].value;
+				_this.host.setImg();
+			});			
+		},
+		buildDom_flash:function() {
+			this.el={};
+			this.el.applyB= $(document.createElement("div")); 
+			this.el.pad= $(document.createElement("div"));
+			var applyB= this.el.applyB;
+			var pad= this.el.pad;
+			applyB.addClass("apply_button");
+			applyB.html("Apply");
+			pad.addClass("control_pad");
+			pad.html('<div class="content">\
+					<div class="tab">\
+					width:<input type="text" value=' + this.host._width + ' />\
+					height:<input type="text" value=' + this.host._height + ' />\
+					</div>\
+					<div class="tab">\
+					url:<input type="text" value="" style="width:170px" />\
+					</div>\
+			</div>');
+			this.setSize();
+			this.setZIndex();
+			this.setPosition();
+			pad.append(applyB);
+			$('body').after(pad);			
+		},
+		buildEvent_flash:function(){
+			var _this= this;
+			this.el.applyB.click(function(e){
+				//调整大小
+				_this.host._width= parseInt(_this.el.pad.find("input")[0].value,10);
+				_this.host._height= parseInt(_this.el.pad.find("input")[1].value,10);
+				_this.host.setSize();
+				_this.host._src= _this.el.pad.find("input")[2].value;
+				_this.host.el.content.html('<embed \
+					src="'+_this.host._src+'" \
+					quality="high" style="width:100%;height:100%" align="middle" \
+					allowScriptAccess="sameDomain" type="application/x-shockwave-flash">\
+				</embed>');
+			});					
 		},
 		reFlow:function() {
 			this.setPosition();
@@ -88,7 +202,7 @@
 		var fn= Class(_ControlPad);
 		return new fn(host,type);
 	};
-	_Card= {
+	var _Card= {
 		init:function(option){
 			this.formatOption(option);
 			this.buildDom();
@@ -111,6 +225,8 @@
 			var cb= this.el.cb;
 			var id= CONFIG.maxID++;
 			this.id= id;
+			//全局维护
+			CONFIG.cards[id]= this;
 			card.addClass("card");
 			cb.addClass("control_button");
 			this.setSize();
@@ -119,14 +235,18 @@
 			if(this._type=="img") {
 				this.el.img= $(document.createElement("img"));
 				var img= this.el.img;
-				img.attr("src",this._src);
+				img.attr("src",this._src||"sample.png");
 				img.attr("alt","card-"+id);
 				card.append(img);
 			}
+			else if(this._type=="flash") {
+				card.html('<div class="content"><div style="text-align:center;"><h1>flash</h1></div></div>');
+			}
 			else {
-				card.html("card-"+id);
+				card.html('<div class="content"><div style="text-align:center;"><h1>text</h1></div></div>');
 			}
 			card.append(cb);
+			this.el.content= $(card.find(".content")[0]);
 			$('body').after(card);
 		},
 		buildEvent:function() {
@@ -141,6 +261,18 @@
 			});
 			*/
 			this.enableDrage();
+		},
+		setImg:function() {
+			this.el.img.attr("src",this._src);
+		},
+		setValue:function() {
+			$(this.el.content).html(this._value);
+		},
+		setFontSize:function() {
+			$(this.el.content).css("fontSize",this._fontSize);
+		},
+		setFontColor:function() {
+			$(this.el.content).css("color",this._fontColor);
 		},
 		setHeight:function() {
 			var card= this.el.card;
@@ -215,15 +347,128 @@
 		pull:function() {
 			//拉参数
 		}
-	}
+	};
+	var _Panel={
+		init:function(option){
+			this.formatOption(option);
+			this.buildDom();
+			this.buildEvent();
+		},
+		formatOption:function(option) {
+			this._left= option.left;
+			this._top= option.top;
+		},
+		buildDom:function(){
+			this.el={};
+			this.el.panel= $(document.createElement("div"));
+			this.el.head= $(document.createElement("div"));
+			this.el.body= $(document.createElement("div"));
+			this.el.foot= $(document.createElement("div"));
+			var body= this.el.body;
+			var head= this.el.head;
+			var panel= this.el.panel;
+			var foot= this.el.foot;
+			head.addClass("head");
+			panel.addClass("panel");
+			body.addClass("body");
+			body.html('\
+				<div class="panel_body_icon panel_body_text" >TEXT</div>\
+				<div class="panel_body_icon panel_body_img" ></div>\
+				<div class="panel_body_icon panel_body_flash" >FLASH</div>\
+			');
+			foot.addClass("foot");
+			panel.append(head);
+			panel.append(body);
+			panel.append(foot);
+			$("body").after(panel);
+			this.setPosition();			
+		},
+		buildEvent:function(){
+			_this= this;
+			var head= this.el.head;
+			var buttons= this.el.body.find("div");
+			head.mousedown(function(e){
+				e.preventDefault();
+				_this.setZIndex();
+				_this._oldX= e.pageX;
+				_this._oldY= e.pageY;
+				_this._pageX= e.pageX;
+				_this._pageY= e.pageY;
+				_this.isShortClick= true;
+				$(window).mousemove(function(e){
+					e.preventDefault();
+					if (Math.abs(e.pageX - _this._oldX) + Math.abs(e.pageY - _this._oldY) > 20) {
+						_this.isShortClick= false;;
+						_this._left = e.pageX - _this._pageX + _this._left;
+						_this._top = e.pageY - _this._pageY + _this._top;
+						_this.setPosition();
+						_this._pageX= e.pageX;
+						_this._pageY= e.pageY;
+					}
+				});
+				$(window).mouseup(function(e){
+					if(_this.isShortClick) {
+						_this.shortClick();
+					}
+					$(window).unbind();
+					_this.isShortClick= true;
+				});
+			});
+			$(buttons).each(function(i,dom){
+				$(dom).mousedown(function(e){
+					e.preventDefault();
+					var clone= $(dom).clone();
+					clone.css("position","absolute");
+					var left= e.pageX-22.5;
+					var top= e.pageY-20;
+					clone.css("left",left);
+					clone.css("top",top);
+					$("body").after(clone);
+					var types= ["text","img","flash"];
+					$(window).mousemove(function(e){
+						e.preventDefault();
+						left = e.pageX - 22.5;
+						top = e.pageY - 20;
+						clone.css("left",left);
+						clone.css("top",top);
+						clone.css("opacity",0.6);
+					});
+					$(window).mouseup(function(e){
+						$(window).unbind();
+						clone.remove();
+						var card1= wrapCard({left:left-50,top:top-50,type:types[i]});
+					});
+				});
+			});
+		},
+		setZIndex:function() {
+			this._zIndex= CONFIG.zIndex++;
+			this.el.panel.css("zIndex",this._zIndex);			
+		},
+		setPosition:function() {
+			var panel= this.el.panel;
+			panel.css("left",this._left);
+			panel.css("top",this._top);
+		},
+		shortClick:function(){
+			this.el.body.toggle();
+		}
+	};
 	var wrapCard= function(option) {
 		var fn= Class(_Card);
 		return new fn(option);
 	};
+	var wrapPanel= function(option) {
+		var fn= Class(_Panel);
+		return new fn(option)
+	};
 	$(document).ready(function() {
   		//ready
+		var panel= wrapPanel({left:20,top:20});
+		/*
 		var card1= wrapCard({left:100,top:100});
 		var card2= wrapCard({left:300,top:400,type:'img',
 			src:'http://www.webdesignerwall.com/wp-content/uploads/2010/03/pleasefixtheiphone.jpg'
 		});
+		*/
 	});
