1b657bdfa8e27eb757ec6074ee304e2dd8d5dfdb
wesdu <pandorid@gmail.com>
2010-05-20 20:45:23 +0800

    a

--- server.js
+++ server.js
@@ -120,6 +120,7 @@ fu.get("/cardsystem.js", fu.staticHandler("cardsystem.js"));
 fu.get("/jquery-1.2.6.min.js", fu.staticHandler("jquery-1.2.6.min.js"));
 fu.get("/img.png", fu.staticHandler("img.png"));
 fu.get("/sample.png", fu.staticHandler("sample.png"));
+fu.get("/avatar.jpg", fu.staticHandler("avatar.jpg"));
 
 
 fu.get("/who", function (req, res) {
